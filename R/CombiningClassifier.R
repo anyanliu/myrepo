845a06af69298888a4d25a44ea35bb1ec9fe5d0e
anyanliu <anyanliu@umich.edu>
2024-11-18 19:57:07 -0500

    Initial commit

--- R/CombiningClassifier.R
+++ R/CombiningClassifier.R
@@ -9,7 +9,13 @@
 
 CombiningClassifier <- function(train_data, train_labels, test_data) {
 
-  train_labels <- as.factor(train_labels)
+  if (!all(train_labels %in% c(0, 1))) {
+    stop("train_labels must only contain 0 and 1")
+  }
+
+  if (!all(names(test_data) == names(train_data))) {
+    stop("test_data columns must match train_data columns")
+  }
 
   library(rpart)
   library(randomForest)
@@ -18,20 +24,20 @@ CombiningClassifier <- function(train_data, train_labels, test_data) {
   library(xgboost)
 
   # Logistic Regression
-  log_model <- glm(train_labels ~ ., data = train_data, family = binomial)
+  log_pred <- ifelse(predict(log_model, test_data, type = "response") > 0.5, 1, 0)
 
   # Decision Tree
-  tree_model <- rpart(train_labels ~ ., data = train_data, method = "class")
+  tree_pred <- as.numeric(as.character(predict(tree_model, test_data, type = "class")))
 
   # Random Forest
-  rf_model <- randomForest(train_data, train_labels, ntree = 300, mtry = 2)
+  rf_pred <- ifelse(predict(rf_model, test_data, type = "response") > 0.5, 1, 0)
 
   # SVM
-  svm_model <- svm(train_labels ~ ., data = train_data, kernel = "radial", cost = 1, gamma = 0.1)
+  svm_pred <- ifelse(predict(svm_model, test_data) > 0.5, 1, 0)
 
   # XGBoost
-  xgb_train <- xgb.DMatrix(data = as.matrix(train_data), label = as.numeric(train_labels) - 1)
-  xgb_model <- xgboost(data = xgb_train, max_depth = 3, nrounds = 100, objective = "binary:logistic", verbose = 0)
+  xgb_pred <- ifelse(predict(xgb_model, as.matrix(test_data)) > 0.5, 1, 0)
+
 
   # predict
   log_pred <- ifelse(predict(log_model, test_data, type = "response") > 0.5, 1, 0)
@@ -51,13 +57,21 @@ CombiningClassifier <- function(train_data, train_labels, test_data) {
     KNN = knn_pred
   )
 
-  # weighted vote to generate result
+  # Weighted vote
   weights <- c(0.2, 0.1, 0.4, 0.2, 0.4, 0.1)
   combined_pred <- apply(predictions, 1, function(row) {
+    row <- row[row %in% c(0, 1)]
+
+    if (length(row) == 0) {
+      return(sample(c(0, 1), 1))
+    }
+
     unique_vals <- unique(row)
     weighted_votes <- sapply(unique_vals, function(val) sum(weights[row == val]))
     unique_vals[which.max(weighted_votes)]
   })
 
+
   return(as.numeric(combined_pred))
 }
+
